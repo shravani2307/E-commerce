7b02faaf973982721a814a092714fbb99e1c6b45
shravani2307 <80692933+shravani2307@users.noreply.github.com>
2023-08-25 13:21:39 +0530

    From src file + admin+user(files)

---
+++ App.js
@@ -0,0 +1,15 @@
+import UserModule from "./user/userapp";
+import SellerModule from "./admin/adminapp";
+
+function App() {
+
+  if (localStorage.getItem("sellerid") == null) {
+    return (<UserModule />);
+  }
+  else {
+    return (<SellerModule />);
+  }
+
+}
+
+export default App;
